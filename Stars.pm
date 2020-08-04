e1d2f7770b4d8d8b3a95020438579ad19f616f82
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-08-04 13:52:05 +0200

    Simplify code

--- Stars.pm
+++ Stars.pm
@@ -27,11 +27,8 @@ sub percent_stars {
 	my $stars_hr = {};
 	foreach my $star_id (1 .. 10) {
 		if ($percent >= $star_id * 10) {
-
 			$stars_hr->{$star_id} = 'full';
-		} elsif ($percent >= ($star_id * 10) - 5
-			&& $percent < $star_id * 10) {
-
+		} elsif ($percent >= ($star_id * 10) - 5) {
 			$stars_hr->{$star_id} = 'half',
 		} else {
 			$stars_hr->{$star_id} = 'nothing',
